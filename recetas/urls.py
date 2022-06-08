3af1e0da7b040865955f2b5f8482ad03d6191020
LuM25 <luciamacarenamolina@gmail.com>
2022-06-08 19:15:57 -0300

    cambios Manuel

--- recetas/urls.py
+++ recetas/urls.py
@@ -4,10 +4,28 @@ from .views import *
 app_name = 'recetas'
 
 urlpatterns = [ 
-    path('', ListaRecetas_1a3.as_view(), name = 'lista_recetas'),
+    path('', ListaRecetas.as_view(), name = 'lista_recetas'),
+
+    path('buscar-receta-1a3/', buscar_receta_1a3, name = 'buscar_receta_1a3'),
+    path('buscar-receta-4a6/', buscar_receta_4a6, name = 'buscar_receta_4a6'),
+    path('buscar-receta-7a10/', buscar_receta_7a10, name = 'buscar_receta_7a10'),
+
     path('crear-receta/', CrearReceta_1a3.as_view(), name='crear_receta'),
     path('crear-receta/', CrearReceta_4a6.as_view(), name='crear_receta'),
     path('crear-receta/', CrearReceta_7a10.as_view(), name='crear_receta'),
-    path('buscar_receta/', buscar_receta, name= 'buscar_receta')
+    
+    path('detail-receta-1a3/<int:pk>/', DetailReceta_1a3.as_view(), name='detail_receta_1a3'),
+    path('detail-receta-4a6/<int:pk>/', DetailReceta_4a6.as_view(), name='detail_receta_4a6'),
+    path('detail-receta-7a10/<int:pk>/', DetailReceta_7a10.as_view(), name='detail_receta_7a10'),
+
+    path('eliminar-receta-1a3/<int:pk>/', EliminarReceta_1a3.as_view(), name='eliminar_receta_1a3'),
+    path('eliminar-receta-4a6/<int:pk>/', EliminarReceta_4a6.as_view(), name='eliminar_receta_4a6'),
+    path('eliminar-receta-7a10/<int:pk>/', EliminarReceta_7a10.as_view(), name='eliminar_receta_7a10'),
+
+    path('actualizar-receta-1a3/<int:pk>/', ActualizarReceta_1a3.as_view(), name='actualizar_receta_1a3'),
+    path('actualizar-receta-4a6/<int:pk>/', ActualizarReceta_4a6.as_view(), name='actualizar_receta_4a6'),
+    path('actualizar-receta-7a10/<int:pk>/', ActualizarReceta_7a10.as_view(), name='actualizar_receta_7a10'),
+
+
 ]
 
--- recetas/views.py
+++ recetas/views.py
@@ -2,45 +2,133 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from recetas.models import Receta_1a3, Receta_4a6, Receta_7a10
 from django.urls import reverse_lazy, reverse
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView, DetailView, DeleteView
 from recetas.forms import FormCrearReceta_1a3, FormCrearReceta_4a6, FormCrearReceta_7a10 
 
 # Create your views here.
 
+########################################################## CREATE VIEW ########################################################################
+
 class CrearReceta_1a3(CreateView):
     model = Receta_1a3
     form_class = FormCrearReceta_1a3
-    template_name = 'crear_receta.html'
+    template_name = 'crear_receta_1a3.html'
     success_url = reverse_lazy('recetas:lista_recetas')
-
-class ListaRecetas_1a3(ListView):
-    model = Receta_1a3
-    template_name= 'recetas.html'
 
 class CrearReceta_4a6(CreateView):
     model = Receta_4a6
-    form_class = FormCrearReceta_1a3
-    template_name = 'crear_receta.html'
+    form_class = FormCrearReceta_4a6
+    template_name = 'crear_receta_4a6.html'
     success_url = reverse_lazy('recetas:lista_recetas')
-
-class ListaRecetas_4a6(ListView):
-    model = Receta_4a6
-    template_name= 'recetas.html'
 
 class CrearReceta_7a10(CreateView):
     model = Receta_7a10
-    form_class = FormCrearReceta_1a3
-    template_name = 'crear_receta.html'
+    form_class = FormCrearReceta_7a10
+    template_name = 'crear_receta_7a10.html'
     success_url = reverse_lazy('recetas:lista_recetas')
 
-class ListaRecetas_7a10(ListView):
-    model = Receta_7a10
-    template_name= 'recetas.html'
+########################################################### LIST VIEW ########################################################################
 
-def buscar_receta(request):
-    receta1_a3 =  Receta_1a3.objects.filter(name__icontains=request.GET['search'])
-    if receta1_a3.exists():
-        context = {'receta_1a3': receta1_a3}
+class ListaRecetas(ListView):
+    model = Receta_1a3
+    template_name= 'recetas.html'
+    
+    def get_context_data(self):
+        context = {}
+        lista_recetas_1a3 = Receta_1a3.objects.all()
+        lista_recetas_4a6 = Receta_4a6.objects.all()
+        lista_recetas_7a10 = Receta_7a10.objects.all()
+        context['lista_recetas_1a3'] = lista_recetas_1a3
+        context['lista_recetas_4a6'] = lista_recetas_4a6
+        context['lista_recetas_7a10'] = lista_recetas_7a10
+        return context
+
+
+########################################################### DETAIL VIEW ######################################################################
+
+class DetailReceta_1a3(DetailView):
+    model = Receta_1a3
+    template_name = 'detail_receta_1a3.html'
+
+class DetailReceta_4a6(DetailView):
+    model = Receta_4a6
+    template_name = 'detail_receta_4a6.html'
+
+class DetailReceta_7a10(DetailView):
+    model = Receta_7a10
+    template_name = 'detail_receta_7a10.html'
+
+########################################################### DELETE VIEW ######################################################################
+
+class EliminarReceta_1a3(DeleteView):
+    model = Receta_1a3
+    template_name = 'eliminar_receta_1a3.html'
+
+    def get_success_url(self):
+        return reverse('lista_recetas')
+
+class EliminarReceta_4a6(DeleteView):
+    model = Receta_4a6
+    template_name = 'eliminar_receta_4a6.html'
+
+    def get_success_url(self):
+        return reverse('lista_recetas')
+
+class EliminarReceta_7a10(DeleteView):
+    model = Receta_7a10
+    template_name = 'eliminar_receta_7a10.html'
+
+    def get_success_url(self):
+        return reverse('lista_recetas')
+
+########################################################### UPDATE VIEW ######################################################################
+
+
+class ActualizarReceta_1a3(UpdateView):
+    model = Receta_1a3
+    template_name = 'actualizar_receta_1a3.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('recetas:detail_receta_1a3', kwargs = {'pk':self.object.pk})
+
+class ActualizarReceta_4a6(UpdateView):
+    model = Receta_4a6
+    template_name = 'actualizar_receta_4a6.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('recetas:detail_receta_4a6', kwargs = {'pk':self.object.pk})
+
+class ActualizarReceta_7a10(UpdateView):
+    model = Receta_7a10
+    template_name = 'actualizar_receta_7a10.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('recetas:detail_receta_7a10', kwargs = {'pk':self.object.pk})
+
+########################################################### SEARCH VIEW ######################################################################
+def buscar_receta_1a3(request):
+    recetas = Receta_1a3.objects.filter(nombre__icontains=request.GET['search'])
+    if recetas.exists():
+        context = {'recetas':recetas}
     else:
-        context = {'errors':'No se encontro la receta'}
-    return render(request, 'buscar_receta.html', context = context)
+        context = {'errors':'No se encontro el producto'}
+    return render(request, 'buscar_receta_1a3.html', context = context)
+
+def buscar_receta_4a6(request):
+    recetas = Receta_4a6.objects.filter(nombre__icontains=request.GET['search'])
+    if recetas.exists():
+        context = {'recetas':recetas}
+    else:
+        context = {'errors':'No se encontro el producto'}
+    return render(request, 'buscar_receta_4a6.html', context = context)
+
+def buscar_receta_7a10(request):
+    recetas = Receta_7a10.objects.filter(nombre__icontains=request.GET['search'])
+    if recetas.exists():
+        context = {'recetas':recetas}
+    else:
+        context = {'errors':'No se encontro el producto'}
+    return render(request, 'buscar_receta_7a10.html', context = context)
