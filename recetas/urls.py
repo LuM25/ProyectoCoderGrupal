394408b70c40ae7a8b79f81f320ea831eee9fca5
LuM25 <luciamacarenamolina@gmail.com>
2022-06-08 19:28:44 -0300

    urls

--- recetas/urls.py
+++ recetas/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('buscar-receta-4a6/', buscar_receta_4a6, name = 'buscar_receta_4a6'),
     path('buscar-receta-7a10/', buscar_receta_7a10, name = 'buscar_receta_7a10'),
 
-    path('crear-receta/', CrearReceta_1a3.as_view(), name='crear_receta'),
-    path('crear-receta/', CrearReceta_4a6.as_view(), name='crear_receta'),
-    path('crear-receta/', CrearReceta_7a10.as_view(), name='crear_receta'),
+    path('crear-receta-1a3/', CrearReceta_1a3.as_view(), name='crear_receta_1a3'),
+    path('crear-receta-4a6/', CrearReceta_4a6.as_view(), name='crear_receta_4a6'),
+    path('crear-receta-7a10/', CrearReceta_7a10.as_view(), name='crear_receta_7a10'),
     
     path('detail-receta-1a3/<int:pk>/', DetailReceta_1a3.as_view(), name='detail_receta_1a3'),
     path('detail-receta-4a6/<int:pk>/', DetailReceta_4a6.as_view(), name='detail_receta_4a6'),
