1447e38247720486a4df336af9e55844c13838b8
Lucia Molina <106121847+LuM25@users.noreply.github.com>
2022-07-03 12:29:36 -0300

    Delete recetas directory

--- recetas/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from recetas.models import Receta_1a3, Receta_4a6, Receta_7a10
-
-# Register your models here.
-
-admin.site.register(Receta_1a3)
-admin.site.register(Receta_4a6)
-admin.site.register(Receta_7a10)
-
